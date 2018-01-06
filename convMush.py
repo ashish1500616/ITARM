305617cf6731214c7e8f78ffa36c71bd7c6c61cc
Ashish Verma <ashish1500616@gmail.com>
2018-01-06 13:16:22 +0530

    convert mushroom.txt to binarymushroom

---
+++ convMush.py
@@ -0,0 +1,37 @@
+import sys 
+
+file1=open("mushroom.txt","r")
+file1.readline()
+
+
+file2=open("convertedmushFile.txt","a+")
+file2.seek(0)
+file2.truncate()
+l = list(file1)
+count=1
+for n in l:
+	var=[0]*120
+	count+=1
+	print(count,"\n")
+	eachline=n.split(" ")
+	del eachline[-1]
+
+	for d in eachline:
+		var[int(d)]=1
+	
+	file2.write(str(var))
+	
+
+
+
+file1.close()
+file2.close()
+
+
+# print((int((list(file1)[1]).split(" ")[0])))
+# sys.exit()
+
+# var=[0]*119
+
+
+# print("val d =",d,"\n"
