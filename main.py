1747f3243d6a08ddef9c46483f9bd6834bb5b2d9
Ashish Verma <ashish1500616@gmail.com>
2018-02-05 15:42:20 +0530

    changing search

--- main.py
+++ main.py
@@ -2,14 +2,17 @@ import csv
 import sys
 import itertools
 import pandas as pd
+# import time
+# start_time = time.time()
+# print("Start time {d}".format(d=start_time))
 # open the csv file containg the data
 
 
 class Frequency(object):
-    def __init__(self, fileName):
+    def __init__(self, fileName,sup):
         self.fileName = fileName
         self.data = []
-        self.support = 45
+        self.support = sup
         self.firstFrequentSet = []
         self.firstCounterset = []
         self.nameOfProducts = []
@@ -40,11 +43,11 @@ class Frequency(object):
 
     def createFirstCandidate(self):
         nameOfProducts = self.data[0][0].split(',')
-        noOfProducts = len(nameOfProducts[1:])
+        noOfProducts = len(nameOfProducts)
         self.firstCounterset = [0] * (noOfProducts + 1)
-        for i in range(1, noOfProducts + 1):
+        for i in range(0, noOfProducts -1 ):
             for j in range(1, len(self.data) - 1):
-                if self.data[j][0].split(',')[i] == "TRUE":
+                if self.data[j][0].split(',')[i] == "1":
                     self.firstCounterset[i] += 1
             if self.firstCounterset[i] > ((self.support / 100) * len(self.data)):
                 self.firstFrequentSet.append(i)
@@ -76,7 +79,9 @@ class Frequency(object):
             self.endPartition))
         # print(self.secondFrequentSupportZip)
         # sys.exit()
-        # creating second candidate Dictionary
+        # creating second candidate Dictionary# import time
+# start_time = time.time()
+# print("Start time {d}".format(d=start_time))
         for i in range(len(self.secondCandidateSet)):
             self.secondCandidateDict[self.secondCandidateSet[i]
                                      ] = list((self.secondCandidateSupport[i],
@@ -94,11 +99,11 @@ class Frequency(object):
     def checkDataAtCell(self, l):
         if(l != ""):
             ind = l
-            if self.data[self.indexCount][0].split(',')[ind] == "TRUE":
+            if self.data[self.indexCount][0].split(',')[ind] == "1":
                 return 1
 
     def filterCandidateSet(self, x):
-        if x[1] > (50):  # ((self.support/100)*len(self.data))
+        if x[1] >  ((self.support/100)*len(self.data)):
             # if x[1]>((self.support/100)*len(self.data)):
             return x[0]
 
@@ -122,14 +127,21 @@ class Frequency(object):
         # print("self.KFrequentSet)
 
     def filterKCandidateSet(self, x):
-        if x[1] > (50):  # ((self.support/100)*len(self.data))
+        if x[1] >  ((self.support/100)*len(self.data)):
             return x
 
 
 
 # minDict={},maxDict={}
-print("Support Variable For File 1 is 50.")
-object = Frequency('MOCK_DATA.csv')
+# MOCK_DATA.csv
+# MOCK_DATA2.csv
+file1=input("Enter the name of the FILE1:::   ")
+file2=input("Enter the name of the FILE2:::   ")
+sup_t=int(input("The support for the FILE1:::   "))
+print("Support Variable For File 1 is {d}".format(d=sup_t ))
+sup2_t=int(input("The support for the FILE2:::   "))
+print("Support Variable For File 1 is {d}.".format(d=sup2_t ))
+object = Frequency(file1,sup_t)
 object.openFile()
 object.createFirstCandidate()
 print("File1 : First Frequent Set {d1}".format(d1=object.firstFrequentSet))
@@ -145,12 +157,18 @@ object.searchKCandidateSet()
 print("\n\n")
 
 
-object2 = Frequency('MOCK_DATA2.csv')
+object2 = Frequency(file2,sup2_t)
 object2.openFile()
 object2.createFirstCandidate()
 print("File2 : First Frequent Set {d1}".format(d1=object2.firstFrequentSet))
 print("\n\n")
-object2.createSecondCandidateSet()
+object2.createSecondCandidateSet()# for key,value in object.secondCandidateDict:
+# 	secondFrequentSet[key]=value
+
+# for key,value in object2.secondCandidateDict:
+# 	secondFrequentSet[key]=value
+
+#print("Second Frequent Set {d1}".format(d1=secondFrequentSet))
 object2.searchSecondCandidatesInTransaction()
 print("File2: Second Candidate Set\n {d1}".format(
     d1=object2.secondCandidateDict))
@@ -167,12 +185,12 @@ else:
 
 print("File2: Creating Second Frequent Set ")
 print("\n\n")
-print("Taken 50 as the support variable for File2.")
+# print("Taken 50 as the support variable for File2.")
 secondFrequentSet = {}
 for key in minDict:
     if key in maxDict:
         val = minDict[key][0] + maxDict[key][0]
-        if val > 50:
+        if val > sup2_t:
             secondFrequentSet[key] =maxDict[key]
             secondFrequentSet[key][0]=val
 
@@ -186,11 +204,11 @@ for key in secondFrequentSet:
 
 temp = list(object.secondCandidateDict.keys())
 for key in temp:
-    if object.secondCandidateDict[key][0] < (0.30*len(object.data)+len(object2.data)):#(initialLen+finalLen)
+    if object.secondCandidateDict[key][0] < ((sup2_t/100)*len(object.data)+len(object2.data)):#(initialLen+finalLen)
         del object.secondCandidateDict[key]
 temp = list(object2.secondCandidateDict.keys())
 for key in temp:
-    if object2.secondCandidateDict[key][0] < (0.30*len(object2.data)):#40:#(finalLen)
+    if object2.secondCandidateDict[key][0] < ((sup2_t/100)*len(object2.data)):#40:#(finalLen)
         del object2.secondCandidateDict[key]
 
 print("File2 :SecondCandidateSet After Deletion{data}".format(
@@ -201,12 +219,3 @@ secondFrequentSet.update(object2.secondCandidateDict)
 secondFrequentSet.update(object.secondCandidateDict)
 
 print("File 2: Second Frequent Set {data}".format(data=secondFrequentSet))
-
-
-# for key,value in object.secondCandidateDict:
-# 	secondFrequentSet[key]=value
-
-# for key,value in object2.secondCandidateDict:
-# 	secondFrequentSet[key]=value
-
-#print("Second Frequent Set {d1}".format(d1=secondFrequentSet))
